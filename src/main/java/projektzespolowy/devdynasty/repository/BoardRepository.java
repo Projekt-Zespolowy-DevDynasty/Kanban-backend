898f13ffa917457194c9f625d370c41961a27ea9
MichallBB <166235@student.uwm.edu.pl>
2024-02-28 23:34:21 +0100

    first

--- src/main/java/projektzespolowy/devdynasty/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package projektzespolowy.devdynasty.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projektzespolowy.devdynasty.models.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
