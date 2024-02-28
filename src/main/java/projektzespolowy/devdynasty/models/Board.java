313f2b773c699f276b39c75975fb26beef633792
MichallBB <166235@student.uwm.edu.pl>
2024-02-28 22:43:48 +0100

    Revert "//"

---
+++ src/main/java/projektzespolowy/devdynasty/models/Board.java
@@ -0,0 +1,13 @@
+package projektzespolowy.devdynasty.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    //
+
+}
