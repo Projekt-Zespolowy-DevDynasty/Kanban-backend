1cb347f4fc54ace4d17b9940a17271cf8c3913b8
DovBrolly <98031672+DovBrolly@users.noreply.github.com>
2024-03-05 20:04:53 +0100

    Test1

--- src/main/java/projektzespolowy/DevdynastyApplication.java
+++ src/main/java/projektzespolowy/DevdynastyApplication.java
@@ -11,3 +11,56 @@ public class DevdynastyApplication {
 	}
 
 }
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+		import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+	@Autowired
+	private TaskRepository taskRepository;
+
+	@GetMapping
+	public List<Task> getAllTasks() {
+		return taskRepository.findAll();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Task createTask(@RequestBody Task task) {
+		return taskRepository.save(task);
+	}
+
+	@PutMapping("/{id}")
+	public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+		Task task = taskRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Nie znaleziono zadania numer: " + id));
+
+		task.setTitle(taskDetails.getTitle());
+		task.setDescription(taskDetails.getDescription());
+		task.setStatus(taskDetails.getStatus());
+
+		return taskRepository.save(task);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteTask(@PathVariable Long id) {
+		Task task = taskRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Nie znaleziono zadania numer: " + id));
+
+		taskRepository.delete(task);
+	}
+}
+
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
