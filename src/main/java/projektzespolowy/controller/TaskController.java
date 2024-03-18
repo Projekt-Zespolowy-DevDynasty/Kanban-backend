91944416a71e11cc4a886223424e254a39a9e4f2
DovBrolly <98031672+DovBrolly@users.noreply.github.com>
2024-03-18 21:28:57 +0100

    Zmiana nazwy Taska

--- src/main/java/projektzespolowy/controller/TaskController.java
+++ src/main/java/projektzespolowy/controller/TaskController.java
@@ -2,6 +2,7 @@ package projektzespolowy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import projektzespolowy.models.Card;
 import projektzespolowy.models.Task;
@@ -9,7 +10,9 @@ import projektzespolowy.repository.CardRepository;
 import projektzespolowy.repository.TaskRepository;
 import projektzespolowy.wyjatki.ResourceNotFoundException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -53,6 +56,25 @@ public class TaskController {
             throw new UnsupportedOperationException("Nie można usunąć tej karty.");
         }
         cardRepository.delete(card);
+    }
+    @PutMapping("/task/{taskId}/rename")
+    public ResponseEntity<Map<String, String>> renameTask(@PathVariable Long taskId, @RequestBody String newName) {
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new ResourceNotFoundException("Nie znaleziono zadania o podanym ID: " + taskId));
+
+        if (newName.trim().isEmpty()) {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Nazwa zadania nie może być pusta ani składać się wyłącznie z białych znaków.");
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        task.setName(newName);
+        taskRepository.save(task);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Nazwa zadania została pomyślnie zaktualizowana.");
+
+        return ResponseEntity.ok(response);
     }
 }
 //Truskawka
